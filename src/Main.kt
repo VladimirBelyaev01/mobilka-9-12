f9811669aaa89975930ad23ef8d0f6e33a8f433d
VladimirBelyaev01 <volodya.belyaev.34@gmail.com>
2026-01-23 21:45:29 +0300

    Create sealed model ModuleResult

--- src/Main.kt
+++ src/Main.kt
@@ -1,8 +1,8 @@
 import modules.EnergyGenerator
 import modules.ResearchLab
+import modules.ModuleResult
 import resources.OutpostResource
 import resources.ResourceManager
-
 /*fun characters.characters.resources.main(){
     val manager = resources.ResourceManager()
     val minerals = resources.OutpostResource(1,"Minerals", 300)
@@ -55,36 +55,32 @@ fun characters.characters.resources.main() {
 }*/
 
 
+fun handleModuleResult(result: ModuleResult) {
+    when (result) {
+        is ModuleResult.Success -> println("УСПЕХ: ${result.message}")
+        is ModuleResult.ResourceProduced -> println("Ресурс произведён: ${result.resourceName} (+${result.amount})")
+        is ModuleResult.NotEnoughResources -> println("Недостаточно ресурсов: ${result.resourceName}")
+        is ModuleResult.Error -> println("ОШИБКА: ${result.reason}")
+    }
+}
+
 fun main() {
     val manager = ResourceManager()
 
-    manager.add(OutpostResource(id = 1, name = "Minerals", amount = 120))
-    manager.add(OutpostResource(id = 2, name = "Gas", amount = 40))
+    manager.add(OutpostResource(id = 1, name = "Minerals", amount = 300))
+    manager.add(OutpostResource(id = 2, name = "Gas", amount = 100))
 
     val generator = EnergyGenerator()
     val lab = ResearchLab()
 
-    generator.performAction(manager)
-    lab.performAction(manager)
+    println("Запуск модулей:")
+    val generatorResult = generator.performAction(manager)
+    val labResult = lab.performAction(manager)
 
-    println()
+    handleModuleResult(generatorResult)
+    handleModuleResult(labResult)
+
+    println("Ресурсы базы:")
     manager.printAll()
-
-
-    fun handleModuleResult() {
-
-        println("\n Запуск модулей ")
-
-        val generatorResult = generator.performAction(manager)
-        val labResult = lab.performAction(manager)
-
-
-        handleModuleResult(generatorResult)
-        handleModuleResult(labResult)
-
-
-        println("\nРесурсы базы:")
-        manager.printAll()
-    }
 }
 
